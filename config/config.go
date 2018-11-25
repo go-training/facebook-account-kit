f4bbfdab869c0a41710bf2e251047130491fe0e4
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-11-25 12:08:26 +0800

    feat: support port config

--- config/config.go
+++ config/config.go
@@ -3,6 +3,11 @@ package config
 type (
 	// Config stores the configuration settings.
 	Config struct {
+		HTTP struct {
+			Host string
+			Port string `envconfig:"PORT" default:"8080"`
+			Root string `default:"/"`
+		}
 		Facebook struct {
 			AppID   string `envconfig:"TEST_FACEBOOK_APP_ID"`
 			Secret  string
--- main.go
+++ main.go
@@ -92,5 +92,5 @@ func main() {
 		}
 	})
 
-	router.Run(":8080")
+	router.Run(":" + conf.HTTP.Port)
 }
